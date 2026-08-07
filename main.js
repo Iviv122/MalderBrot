0968e77e01a9ba2f3e8e4af5f9e5ce2dc34f7fca
Ivan <ivan@MacBook-Air-Ivan.local>
2026-08-07 16:27:54 +0200

    simple malderbrot

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
-const dpr = window.devicePixelRatio;
-const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-const height = window.innerHeight|| document.documentElement.clientHeight ||document.body.clientHeight;
-
 const canvas = document.getElementById("canvas");
 const rect = canvas.getBoundingClientRect();
-const ctx = canvas.getContext("2d",{alpha : false});
+const ctx = canvas.getContext("2d", { alpha: false });
 
-canvas.width = width * dpr;
-canvas.height = height * dpr;
+const width = window.innerWidth;
+const height = window.innerHeight;
+ctx.canvas.width = width;
+ctx.canvas.height = height;
 
-ctx.strokeStyle = "green";
-ctx.beginPath()
-ctx.moveTo(0, 0)
-ctx.lineTo(width, height);
-ctx.lineWidth = 10
-ctx.stroke()
+canvas.width = width;
+canvas.height = height;
+
+const iteration = 1000;
+const max = 2;
+
+
+const getColor = (n) => {
+  return `rgb(0, ${(n * 5) % 256}, 0)`;
+};
 
 const setPixel = (x, y, color) => {
-  x = (width / 2 + x)
-  y = (height / 2 + y)
-  ctx.fillStyle = color
+  ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
+};
+
+
+function mandelbrot(cx, cy) {
+  let z = { x: 0, y: 0 },
+    n = 0,
+    p,
+    d;
+  do {
+    p = {
+      x: Math.pow(z.x, 2) - Math.pow(z.y, 2),
+      y: 2 * z.x * z.y,
+    };
+    z = {
+      x: p.x + cx,
+      y: p.y + cy,
+    };
+    d = z.x * z.x + z.y * z.y;
+    n += 1;
+  } while (d * d <= max * max && n < iteration);
+  return [n, d * d <= max * max];
 }
 
+const colors = new Array(iteration).fill(0).map((_, i) => getColor(i));
+colors[0] = "#000";
 
-for (let i = -5; i <= 5; i++) {
-  for (let j = -5; j <= 5; j++) {
-    setPixel(i, j, "red")
+const screen = Array.from({ length: height }, () => new Array(width).fill(0));
+
+const REAL_SET = { start: -2, end: 1 };
+const IMAGINARY_SET = { start: -1, end: 1 };
+
+function draw() {
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      const y = IMAGINARY_SET.start + (j / height) * (IMAGINARY_SET.end - IMAGINARY_SET.start);
+      const x = REAL_SET.start + (i / width) * (REAL_SET.end - REAL_SET.start);
+      const [m, isOk] = mandelbrot(x, y);
+      screen[j][i] = colors[isOk ? 0 : (m % colors.length) - 1 + 1];
+    }
+  }
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      setPixel(i, j, screen[j][i]);
+    }
   }
 }
-
-let z = 0
+draw();
