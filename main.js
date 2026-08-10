1f5c630b7d7322aa4195a62b1d5c0206426526ca
Ivan <ivan@MacBook-Air-Ivan.local>
2026-08-10 16:07:43 +0200

    async

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 const canvas = document.getElementById("canvas");
 const rect = canvas.getBoundingClientRect();
 const ctx = canvas.getContext("2d", { alpha: false });
+
 const width = window.innerWidth;
 const height = window.innerHeight;
+
 ctx.canvas.width = width;
 ctx.canvas.height = height;
 canvas.width = width;
 canvas.height = height;
 
-const iteration = 5000;
+const iteration = 256;
 const limit = 2;
+let reset_id = 0;
 
 var Module = {
   onRuntimeInitialized: function () {
-    draw();
+    draw(reset_id);
   },
 };
 
@@ -21,12 +24,12 @@ const colors = new Array(256)
   .fill(0)
   .map((_, val) => `rgb(0, ${val % 256}, ${val % 256})`);
 
-const clear = () => {
+const clear = async () => {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, width, height);
 };
 
-const drawLine = (off, x, y, color) => {
+const drawLine = async (off, x, y, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(off, y, x, 1);
 };
@@ -36,11 +39,17 @@ let IMAGINARY_SET = { start: -1, end: 1 };
 
 const worker = new Worker("worker.js");
 
-worker.addEventListener("message", ({ data }) => {
+worker.addEventListener("message", async ({ data }) => {
+  if (data.reset_id !== reset_id) {
+    return;
+  }
+
   const res = new Uint32Array(data.res);
+
   let w = 1;
   let off = 0;
   let last = res[0];
+
   for (let x = 1; x < width; x++) {
     if (res[x] === last) {
       w++;
@@ -48,22 +57,30 @@ worker.addEventListener("message", ({ data }) => {
       if (res[x - 1] > 0) {
         drawLine(off, w, data.j, colors[res[x - 1] % colors.length]);
       }
+
       off += w;
       w = 1;
+      last = res[x];
     }
+  }
+
+  // Draw the final run.
+  if (res[width - 1] > 0) {
+    drawLine(off, w, data.j, colors[res[width - 1] % colors.length]);
   }
 });
 
 const bytesPerPixel = Uint32Array.BYTES_PER_ELEMENT;
-function compute_row(j) {
+
+async function compute_row(j, curr_id) {
   const buffer = crossOriginIsolated
     ? new SharedArrayBuffer(width * bytesPerPixel)
     : new ArrayBuffer(width * bytesPerPixel);
 
   worker.postMessage({
-    buffer: buffer,
-    j: j,
-    width: width,
+    buffer,
+    j,
+    width,
     realStart: REAL_SET.start,
     realEnd: REAL_SET.end,
     im:
@@ -71,24 +88,32 @@ function compute_row(j) {
       (j / height) * (IMAGINARY_SET.end - IMAGINARY_SET.start),
     l: limit,
     it: iteration,
+    reset_id: curr_id,
   });
 }
 
-function draw() {
-  clear();
-  for (let i = 0; i < height; i++) {
-    compute_row(i);
+async function draw(curr_id) {
+  for (let i = 0; i < height; i += 1) {
+    if (reset_id !== curr_id) {
+      return;
+    }
+    compute_row(i, curr_id);
   }
 }
 
 let ZOOM_FACTOR = 0;
 
-canvas.addEventListener("wheel", (e) => {
+canvas.addEventListener("wheel", async (e) => {
+  e.preventDefault();
+  reset_id++;
+  clear();
+
   if (e.deltaY > 0) {
     ZOOM_FACTOR = 0.1;
   } else {
     ZOOM_FACTOR = 2;
   }
+
   const zfw = width * ZOOM_FACTOR;
   const zfh = height * ZOOM_FACTOR;
 
@@ -96,16 +121,14 @@ canvas.addEventListener("wheel", (e) => {
     start: getRelativePoint(e.pageX - zfw, width, REAL_SET),
     end: getRelativePoint(e.pageX + zfw, width, REAL_SET),
   };
+
   IMAGINARY_SET = {
     start: getRelativePoint(e.pageY - zfh, height, IMAGINARY_SET),
     end: getRelativePoint(e.pageY + zfh, height, IMAGINARY_SET),
   };
 
-  clear();
-  draw();
+  draw(reset_id);
 });
 
 const getRelativePoint = (pixel, length, set) =>
   set.start + (pixel / length) * (set.end - set.start);
-
-draw();
--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 var Module = {
-  onRuntimeInitialized: function () {
+  onRuntimeInitialized: async function () {
     moduleReady = true;
     while (queue.length > 0) {
       Process_Row(queue.shift());
@@ -10,8 +10,12 @@ importScripts("./lib.js");
 
 const queue = [];
 let moduleReady = false;
+let reset_id = 0;
 
-function Process_Row(data) {
+async function Process_Row(data) {
+  if (data.reset_id < reset_id) {
+    return;
+  }
   const res = new Uint32Array(data.buffer);
   Module.processRow(
     res,
@@ -23,10 +27,22 @@ function Process_Row(data) {
     data.l,
     data.it,
   );
-  postMessage({ j: data.j, res: res });
+
+  if (data.reset_id < reset_id) {
+    return;
+  }
+  postMessage({ j: data.j, res: res, reset_id: data.reset_id });
 }
 
-onmessage = ({ data }) => {
+async function reset() {
+  queue.length = 0;
+}
+
+onmessage = async ({ data }) => {
+  if (data.reset_id < reset_id) {
+    return;
+  }
+  reset_id = data.reset_id;
   if (!moduleReady) {
     queue.push(data);
     return;
